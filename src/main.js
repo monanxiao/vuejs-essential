19cdb4de1929147d7140d5d0e27cdc91ef445c40
monanxiao <monanxiao@qq.com>
2018-06-14 16:23:44 +0800

    有bug ，暂未修改

--- src/main.js
+++ src/main.js
@@ -1,16 +1,63 @@
-// 引入 vue.js 的默认值
 import Vue from 'vue'
-// 引入 App.vue 的默认值
 import App from './App'
+import router from './router'
+import './directives'
+import './components'
+// 引入 store/index.js 的默认值
+import store from './store'
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+// 引入插件
+import Message from './plugins/message'
 
-// 设置 false 以阻止 Vue 在启动时生成生产提示
+import './filters'
+
+import { mockArticles } from './mock/data'
+
+import ls from './utils/localStorage'
+// 运行 ./mock/index.js
+import './mock'
+// 引入 axios 的默认值
+import axios from 'axios'
+
+// 将 axios 添加到 Vue.prototype 上，使其在实例内部的所有组件中可用
+Vue.prototype.$axios = axios
+
+// 使用插件
+Vue.use(Message)
+
+// 使用插件
+Vue.use(VueSweetalert2)
+
 Vue.config.productionTip = false
 
-// eslint 配置，允许 new 一个实例后不赋值，我们没有使用 eslint，如果有，则下一行注释不可缺少
+const AddMockData = (() => {
+  // 是否加入测试数据
+  const isAddMockData = true
+  // 用户数据
+  let userArticles = ls.getItem('articles')
+
+  if (Array.isArray(userArticles)) {
+    userArticles = userArticles.filter(article => parseInt(article.uid) === 1)
+  } else {
+    userArticles = []
+  }
+
+  if (isAddMockData) {
+    // 合并用户数据和测试数据，使用合并值作为所有文章
+    store.commit('UPDATE_ARTICLES', [...userArticles, ...mockArticles(60)])
+  } else {
+    // 使用用户数据作为所有文章
+    store.commit('UPDATE_ARTICLES', userArticles)
+  }
+})()
+
 /* eslint-disable no-new */
-// 创建一个新的 Vue 实例
 new Vue({
   el: '#app',
+  router,
+  // 注入 store
+  store,
   components: { App },
   template: '<App/>'
 })
